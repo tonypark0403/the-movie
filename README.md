97630ee752b6f483c92847e891e1ca1b9b8b9ee2
Tony Park <c3dream@gmail.com>
2019-01-27 21:05:29 -0500

    added hash router

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # the-movie
+
+With React and ES6 from the move api.
+
+## Screens
+
+- [ ] Home
+- [ ] TV Shows
+- [ ] Search
+- [ ] Detail
--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Router from "Components/Router";
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				App
-      </div>
+			<>
+				<Router />
+			</>
 		);
 	}
 }
